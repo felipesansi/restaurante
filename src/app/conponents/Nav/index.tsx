f074fea236125297ac7b383d103a272f46cdf1bd
Felipe <felipeprofissional22@gmail.com>
2024-04-24 21:51:21 -0300

    Aula react

---
+++ src/app/conponents/Nav/index.tsx
@@ -0,0 +1,15 @@
+export const Menu = ()=>{
+    return(
+        <header>
+            <div>
+
+            </div>
+            <nav>
+                <li><a href="#"></a>Home</li>
+                <li><a href="#"></a>Cardápio</li>
+                <li><a href="#"></a>Pedidos</li>
+                <li><a href="#"></a>Contatos</li>
+            </nav>
+        </header>
+    )
+}
